daa6952c2a76b33e7103f74debdd86f4ce3b14ed
Iarlen <iarlensilva775@gmail.com>
2018-01-13 18:23:15 -0300

    Criando a variável cursos

--- src/app/diretiva-ng-for/diretiva-ng-for.component.ts
+++ src/app/diretiva-ng-for/diretiva-ng-for.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiretivaNgForComponent implements OnInit {
 
+  cursos: string[] = ['Java', 'Angular', 'JS'];
   constructor() { }
 
   ngOnInit() {
