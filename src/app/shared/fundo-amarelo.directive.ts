42ca98fad297fc166b6f6cc78818567821caeba0
Iarlen <iarlensilva775@gmail.com>
2018-01-15 21:13:01 -0300

    Definindo a cor do fundo

--- src/app/shared/fundo-amarelo.directive.ts
+++ src/app/shared/fundo-amarelo.directive.ts
@@ -6,7 +6,8 @@ import { Directive, ElementRef } from '@angular/core';
 export class FundoAmareloDirective {
 
   constructor(private elementRef: ElementRef) {
-    console.log(this.elementRef);
+    //console.log(this.elementRef);
+    this.elementRef.nativeElement.style.backgroundColor = 'yellow';
   }
 
 }
