6790bd692a47e2cb034c0445b2a42f71003fd8ff
Iarlen <iarlensilva775@gmail.com>
2018-01-15 21:19:33 -0300

    Usando as boas práticas com o renderer

--- src/app/shared/fundo-amarelo.directive.ts
+++ src/app/shared/fundo-amarelo.directive.ts
@@ -1,13 +1,15 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Renderer } from '@angular/core';
 
 @Directive({
   selector: '[FundoAmarelo]' //colocar a tag na frente para definir exclusividade ex.: p[FundoAmarelo]
 })
 export class FundoAmareloDirective {
 
-  constructor(private elementRef: ElementRef) {
+  constructor(private elementRef: ElementRef, private renderer: Renderer) {
     //console.log(this.elementRef);
-    this.elementRef.nativeElement.style.backgroundColor = 'yellow';
+    //this.elementRef.nativeElement.style.backgroundColor = 'yellow';
+    this.renderer.setElementStyle(this.elementRef.nativeElement,
+    'background-color', 'yellow');
   }
 
 }
