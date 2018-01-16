e1e9011a27e25aa661ac8c6b07537cfa6a9a711c
Iarlen <iarlensilva775@gmail.com>
2018-01-15 21:34:29 -0300

    Trocando o background color ao sair do p

--- src/app/shared/highlight-mouse.directive.ts
+++ src/app/shared/highlight-mouse.directive.ts
@@ -5,9 +5,14 @@ import { Directive, HostListener, ElementRef, Renderer } from '@angular/core';
 })
 export class HighlightMouseDirective {
 
-  @HostListener('mouseenter') onMouseOvrt () {
+  @HostListener('mouseenter') onMouseOver () {
     this.renderer.setElementStyle(this.elementRef.nativeElement,
     'background-color', 'yellow');
+  }
+
+  @HostListener('mouseleave') onMouseLeave () {
+    this.renderer.setElementStyle(this.elementRef.nativeElement,
+    'background-color', 'white');
   }
 
   constructor(private elementRef: ElementRef, private renderer: Renderer) { }
