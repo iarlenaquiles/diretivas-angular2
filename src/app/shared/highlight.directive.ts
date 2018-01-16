37a718b56cde59d22272c5357435101b1ef64bab
Iarlen <iarlensilva775@gmail.com>
2018-01-15 22:18:14 -0300

    Iniciando o background pelo oninit

--- src/app/shared/highlight.directive.ts
+++ src/app/shared/highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, HostListener, HostBinding } from '@angular/core';
+import { Directive, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[Highlight]'
@@ -6,15 +6,24 @@ import { Directive, HostListener, HostBinding } from '@angular/core';
 export class HighlightDirective {
 
   @HostListener('mouseenter') onMouseOver () {
-    this.backgroundColor = 'yellow';
+    this.backgroundColor = this.highlightColor;
   }
 
   @HostListener('mouseleave') onMouseLeave () {
-    this.backgroundColor = 'white';
+    this.backgroundColor = this.defaultColor;
   }
 
   @HostBinding('style.backgroundColor') backgroundColor: string;
 
+
+  @Input() defaultColor: string = 'white';
+
+  @Input() highlightColor: string = 'yellow';
+
   constructor() { }
+
+  ngOnInit() {
+    this.backgroundColor = this.defaultColor;
+  }
 
 }
