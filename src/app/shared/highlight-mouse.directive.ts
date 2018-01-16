34023443c789d44f8790398c751062aff68a25c0
Iarlen <iarlensilva775@gmail.com>
2018-01-15 21:30:19 -0300

    Usando o HostListener

--- src/app/shared/highlight-mouse.directive.ts
+++ src/app/shared/highlight-mouse.directive.ts
@@ -1,9 +1,13 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener, ElementRef, Renderer } from '@angular/core';
 
 @Directive({
   selector: '[HighlightMouse]'
 })
 export class HighlightMouseDirective {
+
+  @HostListener('mouseenter') onMouseOvrt () {
+
+  }
 
   constructor() { }
 
